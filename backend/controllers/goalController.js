25396cab1dc6daa57cd40eae24a03774b9896089
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-02-18 13:54:26 +0530

    throwing Error and OverRide the Exprees Error Hnadler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -9,7 +9,8 @@
    const setGoal = (req, res) => {
    console.log(req.body);
    if (!req.body.text) {
-      res.status(400).json({ message: "Please add a text field" });
+      res.status(400)
+      throw new Error('Please add a text field')
    }
    res.status(200).json({ message: "Set goal" });
    };
