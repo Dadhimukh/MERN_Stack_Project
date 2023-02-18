e1301bab114ba7e68f86104e609a6ceb10b9c868
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-02-18 12:49:40 +0530

    create controller folder and move all goalRoutes to goalController

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log("Hello workd")
+const express = require("express")
+const dotenv = require('dotenv').config()
+
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.use("/api/goals" , require("./routes/goalRoutes"))
+
+app.listen(port , ()=>{console.log(`Server started on port ${port}`)})
