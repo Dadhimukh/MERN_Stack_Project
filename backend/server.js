4df5dd5d8c7cf733f1d6a9d2d63020bad2440dc8
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-02-18 12:51:28 +0530

    Formated

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
-const express = require("express")
-const dotenv = require('dotenv').config()
+   const express = require("express");
+   const dotenv = require("dotenv").config();
 
-const port = process.env.PORT || 3000
+   const port = process.env.PORT || 3000;
 
-const app = express()
+   const app = express();
 
-app.use("/api/goals" , require("./routes/goalRoutes"))
+   app.use("/api/goals", require("./routes/goalRoutes"));
 
-app.listen(port , ()=>{console.log(`Server started on port ${port}`)})
+   app.listen(port, () => {
+   console.log(`Server started on port ${port}`);
+   });
