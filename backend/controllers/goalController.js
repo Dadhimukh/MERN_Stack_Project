30245ed6cb589c4670dc64aa010003b244886b83
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-02-18 12:58:17 +0530

    Adding comments to routes

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,15 +1,23 @@
+   // @desc    Get goals
+   // @route    GET /api/goals
    const getGoals = (req, res) => {
    res.status(200).json({ message: "Get goals" });
    };
 
+   // @desc    Set goal
+   // @route    SET /api/goal
    const setGoal = (req, res) => {
    res.status(200).json({ message: "Set goal" });
    };
 
+   // @desc    Update goal
+   // @route    PUT /api/goals/:id
    const updateGoal = (req, res) => {
    res.status(200).json({ message: `Update goal ${req.params.id}` });
    };
 
+   // @desc    Delete goal
+   // @route    DELETE /api/goals/:id
    const deleteGoal = (req, res) => {
    res.status(200).json({ message: `Delete goal ${req.params.id}` });
    };
